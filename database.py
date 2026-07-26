9165cf7018a4a4fbabdad4323f255d00f5f1ff8a
maha-NM <smahalakshmi1199@gmail.com>
2026-07-26 12:47:26 +0530

    database.py

--- database.py
+++ database.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database="ai_document_qa"
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
